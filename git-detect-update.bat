cdb52eba7d96ecacdce0a796a786c7dbcef57260
Jack Burch <jburch555@gmail.com>
2023-09-08 20:11:05 -0400

    Update git-detect-update.bat

--- git-detect-update.bat
+++ git-detect-update.bat
@@ -4,7 +4,7 @@ IF %2.==. (SET branch=master) ELSE (SET branch=%2)
 IF %3.==. (SET owner=${DEFAULT_GITHUB_USER}) ELSE (SET owner=%3)
 
 FOR /f %%i IN ('git ls-remote https://github.com/%owner%/%gitrepo%.git %branch%')DO SET "_remote=%%~i"
-PUSHD ${BASE_DIR}\%repo%
+PUSHD ${DEPLOYMENT_DIR}\%repo%
 FOR /f %%i IN ('git rev-parse %branch%')DO SET "_local=%%~i"
 IF %_remote%==%_local% (ECHO 1) ELSE (ECHO 0)
 
